71a4e1dbff4b0ea60b81558139144d9864f6d652
William-McGonagle <mcgonaglew@gfacademy.org>
2021-05-23 16:18:19 -0400

    Added Article Page

---
+++ article/article.ts
@@ -0,0 +1,20 @@
+window.onload = function () {
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const q = urlParams.get('q');
+
+    if (q == undefined || q == null) window.location.href = "/";
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "https://brew-society.herokuapp.com/article/" + q);
+
+    xhr.onreadystatechange = function () {
+         
+        document.getElementById("output").innerHTML = JSON.parse(this.responseText).body;
+
+    };
+
+    xhr.send();
+
+}; 
