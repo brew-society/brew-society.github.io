ad28b40a7593e95466e27a3f9fa34ad1f2aa23b6
William-McGonagle <mcgonaglew@gfacademy.org>
2021-07-23 19:42:06 -0400

    ✨ Connected Topbar to API

--- src/header.ts
+++ src/header.ts
@@ -1,5 +1,33 @@
 var hamburgerToggled = false;
 
+window.addEventListener('load', function () {
+ 
+    var container = document.getElementById('concepts');
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "https://brew-society.herokuapp.com/header");
+
+    xhr.onreadystatechange = function () { 
+        
+        if (xhr.readyState !== 4) return;
+
+        var response = JSON.parse(this.responseText);
+
+        console.log(response);
+
+        for (var i = 0; i < response.length; i++) {
+
+            appendCourse(response[i], container);
+
+        }
+
+    };
+
+    xhr.send(); 
+
+});
+
 document.getElementById('hamburger').onclick = function () {
 
     if (hamburgerToggled) {
@@ -13,5 +41,18 @@ document.getElementById('hamburger').onclick = function () {
     }
 
     hamburgerToggled = !hamburgerToggled;
+
+}
+
+function appendCourse(data, container) {
+
+    //   <a href="/course/">Frontend Building</a>
+
+    var link = document.createElement("A");
+
+    link.innerHTML = data.title;
+    link.setAttribute('href', '/course/?q=' + data.id);
+
+    container.append(link);
 
 }
